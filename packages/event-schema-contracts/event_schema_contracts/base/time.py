c6a8e31c6ebb1c66b40ac983507a4c68807fdb3b
Nicolas Thompson <nicolasgthompson@gmail.com>
2026-03-27 09:01:15 +0000

    refactor(time): enforce ClassVar utc field contracts and switch to offset-safe UTC validation

--- packages/event-schema-contracts/event_schema_contracts/base/time.py
+++ packages/event-schema-contracts/event_schema_contracts/base/time.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-from typing import Any
+from typing import Any, ClassVar
 
 from pydantic import BaseModel, field_validator
 
@@ -10,22 +10,30 @@ class UTCTimestampModel(BaseModel):
     Subclasses define which fields must be UTC using __utc_fields__. 
     """
 
-    __utc_fields__: tuple [str, ...] = ()
+    __utc_fields__: ClassVar[tuple[str, ...]] = ()
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        if not isinstance(cls.__utc_fields__, tuple):
+            raise TypeError("__utc_fields__ must be tuple[str, ...]")
 
     @field_validator("*")
     @classmethod
     def validate_utc_fields(cls, value: Any, info):
+        
         if (
             isinstance(value, datetime)
             and info.field_name in cls.__utc_fields__
         ):
+            if not isinstance(value, datetime):
+                return value
+            
             if value.tzinfo is None:
                 raise ValueError(
                     f"{info.field_name} must be timezone-aware"
                 )
+
+            if value.utcoffset() != timezone.utc.utcoffset(value):
+                raise ValueError(f"{info.field_name} must be UTC")
             
-            if value.tzinfo != timezone.utc:
-                raise ValueError(
-                    f"{info.field_name} must be UTC"
-                ) 
         return value
